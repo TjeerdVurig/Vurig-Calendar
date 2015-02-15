5ee279dd9eb76bb2c70b48de1e34e1b1e06a14e5
XueFei Yang <shippo.yxf@gmail.com>
2015-02-15 14:11:09 -0400

    Migrated to Xcode 6 and iOS SDK 8.1
    Fixed all deprecated code warning.
    
    Fixed all 64-bit CPU support code warning.
    
    added Multi-resolution support.

---
+++ Vurig-CalendarTests/Vurig_CalendarTests.m
@@ -0,0 +1,40 @@
+//
+//  Vurig_CalendarTests.m
+//  Vurig-CalendarTests
+//
+//  Created by XueFei Yang on 2015-02-15.
+//  Copyright (c) 2015 XueFei Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Vurig_CalendarTests : XCTestCase
+
+@end
+
+@implementation Vurig_CalendarTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
